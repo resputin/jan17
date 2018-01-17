67dc5a36dd9d5ca9867e2418e5d17245adeb431b
Evan Garrett <evang522@gmail.com>
2018-01-17 12:00:28 -0500

    Finished object exercises

--- object-drills-1.js
+++ object-drills-1.js
@@ -1,27 +1,64 @@
-'use strict';
+// 'use strict';
 
-function createMyObject() {
-  return {
-    foo: 'bar',
-    answerToUnivers: 42,
-    'ollie ollie': 'oxenfree',
-    sayHello: function() {
-      console.log("Hello");
-    }
-  };
-}
+// function createMyObject() {
+//   return {
+//     foo: 'bar',
+//     answerToUnivers: 42,
+//     'ollie ollie': 'oxenfree',
+//     sayHello: function() {
+//       console.log("Hello");
+//     }
+//   };
+// }
 
-const newObject = createMyObject();
+// const newObject = createMyObject();
 
-newObject.sayHello();
+// newObject.sayHello();
 
-function updateObject(obj) {
-  obj.foo = 'foo';
-  obj.bar = 'bar';
-  obj.bizz = 'bizz';
-  obj.bang = 'bang';
-}
+// function updateObject(obj) {
+//   obj.foo = 'foo';
+//   obj.bar = 'bar';
+//   obj.bizz = 'bizz';
+//   obj.bang = 'bang';
+// }
 
-updateObject(newObject);
+// updateObject(newObject);
 
-console.log(newObject);
+// console.log(newObject);
+
+
+// Self Reference
+
+// function personMaker() {
+//     var person = {
+//       firstName: 'Paul',
+//       lastName: 'Jones',
+//       // replace `null` with a function that uses self reference to return
+//       // full name
+//       fullName: function() {
+//           console.log(this.firstName + ' ' + this.lastName);
+//       }
+//     };
+//     return person;
+//   }
+//  const person = personMaker();
+
+//  person.fullName();
+
+
+// function keyDeleter(obj) {
+//     delete obj.foo;
+//     delete obj.bar;
+//     return obj;
+//   }
+  
+//   var sampleObj = {
+//     foo: 'foo',
+//     bar: 'bar',
+//     bizz: 'bizz',
+//     bang: 'bang',
+//   };
+  
+//  const modifiedObj =  keyDeleter(sampleObj);
+
+//  console.log(modifiedObj);
