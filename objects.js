a746b096ed67114405800dc55cce53df0fba4b77
Dylan <nielsen.dylan@gmail.com>
2018-01-17 11:18:38 -0800

    first two functions of object drills

---
+++ objects.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// const loaf = {
+//   flour: 300,
+//   water: 210,
+//   hydration: function() {
+//     return ((this.water / this.flour) * 100);
+//   }
+// };
+
+// console.log(loaf.flour);
+// console.log(loaf.water);
+// console.log(loaf.hydration());
+
+// const myCoolObject = {
+//   foo: 'foo',
+//   bar: 20,
+//   fum: 'what',
+//   quux: 'quux',
+//   spam: 'email'
+// };
+
+// for (const key in myCoolObject) {
+//   console.log(`${key}: ${myCoolObject[key]}`);
+// }
